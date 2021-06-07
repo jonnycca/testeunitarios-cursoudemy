060eca3eaa00bd806ed693cc8324b3546324a051
Jonny Costa Alves <jcostaal@everis.com>
2021-06-07 18:39:58 -0300

    upload project

---
+++ src/main/java/br/com/servicos/LocacaoService.java
@@ -0,0 +1,30 @@
+package br.com.servicos;
+
+import static br.com.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import br.com.entidades.Filme;
+import br.com.entidades.Locacao;
+import br.com.entidades.Usuario;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar metodo para salvar
+		
+		return locacao;
+	}
+}
---
+++ src/test/java/br/com/servicos/LocacaoServiceTest.java
@@ -0,0 +1,36 @@
+package br.com.servicos;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import br.com.entidades.Filme;
+import br.com.entidades.Locacao;
+import br.com.entidades.Usuario;
+import br.com.utils.DataUtils;
+
+public class LocacaoServiceTest {
+
+	@Test
+	public void teste() {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario1");
+		Filme filme = new Filme("Filme1", 3, 5.0);
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//validacao
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		assertThat(locacao.getValor(), is(not(7.0)));
+		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		
+	}
+}
